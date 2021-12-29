c9576cdd1f3cac090aa3d646bcea70e0f42c0d0a
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-29 17:21:53 -0300

    feat: add verifyConditions and success steps

--- packages/semrel-react-releaser/index.js
+++ packages/semrel-react-releaser/index.js
@@ -1,15 +1,58 @@
+const path = require('path');
+const fs = require('fs-extra');
+const childProcess = require('node:child_process');
 
-function verifyConditions(pluginConfig, context) {
-	const {logger} = context
-	logger.info('verifyConditions step custom plugin')
-	verified = true
+const PREFIX = 'SEMREL-REACT-RELEASER';
+
+let verified = false;
+
+function getLernaPackages() {
+  try {
+    const stdout = childProcess.execSync('lerna ls --all --json').toString();
+    return stdout;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function success() {
+let reactPackagePath;
 
+async function verifyConditions(pluginConfig, context) {
+  const { logger } = context;
+
+  const packageJson = require(path.join(context.cwd, 'package.json'));
+
+  logger.info(PREFIX, packageJson.name);
+
+  logger.info(PREFIX, getLernaPackages());
+
+  reactPackagePath = JSON.parse(getLernaPackages()).find(
+    (pkg) => pkg.name === `react-${packageJson.name}`
+  ).location;
+
+  if (!reactPackagePath) {
+    return;
+  }
+
+  logger.info(PREFIX, reactPackagePath);
+  logger.success(PREFIX, 'react package exists!');
+
+  // const reactPackageExists = fs.existsSync(reactPackagePath);
+
+  verified = true;
 }
 
+function success(pluginConfig, context) {
+  const reactPackageJson = fs.readFileSync(
+    path.join(reactPackagePath, 'package.json')
+  );
+
+  reactPackageJson.version = context.nextRelease.version;
+
+  fs.writeFileSync(path.join(reactPackagePath, 'package.json'));
+}
 
 module.exports = {
-	success
-}
+  success,
+  verifyConditions,
+};
