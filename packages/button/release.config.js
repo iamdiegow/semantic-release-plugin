3bc421d736e9b797aefcb10d7568cd0551002b5c
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-29 16:07:11 -0300

    chore: add semantic-release

---
+++ packages/button/release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	branches: [
+		'main'
+	],
+	plugins: [
+		'semrel-react-releaser'
+	],
+	dryRun: true,
+	ci: false,
+	debug: true
+}
---
+++ packages/semrel-react-releaser/index.js
@@ -0,0 +1,15 @@
+
+function verifyConditions(pluginConfig, context) {
+	const {logger} = context
+	logger.info('verifyConditions step custom plugin')
+	verified = true
+}
+
+function success() {
+
+}
+
+
+module.exports = {
+	success
+}
