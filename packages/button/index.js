831f54e9831dcdccebcc7b50cfcd2696bd86ae8d
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-29 18:39:52 -0300

    fix: update button

--- packages/button/index.js
+++ packages/button/index.js
@@ -1,3 +1,3 @@
 module.exports = function button() {
-  return 'button component updated again and again and again and again and again';
+  return 'button component updated again and again and again and again and again again';
 };
